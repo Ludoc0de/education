f5441af3957a1a1630b0b95fa79d1b60421872f1
ludoc0de <champi.ludo@tutanota.com>
2025-03-31 21:58:01 -0300

    up

--- login.php
+++ login.php
@@ -2,20 +2,7 @@
 $postData = $_POST;
 
 $loginMessage=null;
-if (
-    !isset($postData["email"]) 
-    || !isset($postData["password"])
-    || empty($postData["email"])
-    || !filter_var($postData["email"], FILTER_VALIDE_EMAIL)
-    || empty($postData["password"])
-    ){
-        $loginMessage="Merci de renseigner vos id";
-} else {
-    $loginMessage="Ok id";
-}
-
-// $loginMessage = null;
-
+$loginMessage="ok";
 // if (!empty($_POST['nickname']) && !empty($_POST['pass'])) {
 //     checkLogin($_POST['nickname'], $_POST['pass']);
 //     $loginMessage = "identifiant ou mot de passe incorrect";
@@ -41,8 +28,17 @@ if (
         <?php require_once(__DIR__ .'/header.php'); ?>
     </header>
     <section class="row mt-5">
-        <form class="" action="" method="GET">
+        <form class="" action="" method="POST">
             <h1>Se connecter</h1>
+            <?php 
+            if($loginMessage){  
+            ?>
+            <p class="">
+                <?php echo $loginMessage?>
+            </p>
+            <?php
+            }
+            ?>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input class="form-control" type="text" name="email" placeholder="email">
@@ -51,18 +47,12 @@ if (
                 <label for="password">Mot de passe</label>
                 <input class="form-control" type="password" name="password" placeholder="mot de passe">
             </div>
+            <?php if($loginMessage){?>
             <button type="submit" class="btn btn-dark button-login">Se connecter</button>
-        </form>
-        <?php 
-        if($loginMessage){  
-        ?>
-        <p class="">
-            test: <?php echo $loginMessage?>
-        </p>
-        <?php
+            <?php
         }
         ?>
-
+        </form>
         <div class="" id="">
             <ul class="">
                 <li class="nav-item">
