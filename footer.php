fcb6098780ba9a17aa30b4afe585e8ce025a4693
ludoc0de <champi.ludo@tutanota.com>
2025-04-09 18:14:10 -0300

    clean

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-light text-center text-lg-start mt-auto">
-    <div class="text-center p-3">
-        © 2025 Copyright:
-        <a class="text-dark" href="http://localhost/education/">Éducation Cyber Mioche</a>
-    </div>
-</footer>
--- variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-$usersStatement = $mysqlClient->prepare('SELECT * FROM users');
-$usersStatement->execute();
-$users = $usersStatement->fetchAll();
-
-$moviesStatement = $mysqlClient->prepare('SELECT * FROM movies');
-$moviesStatement->execute();
-$movies = $moviesStatement->fetchAll();
